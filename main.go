266a91f1e2065e916abdb41d63bcd0c187d2b808
guventuncay <guven.tuncay01@gmail.com>
2024-12-01 22:28:17 +0300

    init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"os/exec"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	name := request.QueryStringParameters["name"]
+	if name == "" {
+		name = "World"
+	}
+
+	cmd := exec.Command("./bin/ffmpeg", "-version")
+	output, err := cmd.CombinedOutput()
+
+	response := Response{
+		Message: fmt.Sprintf("Hello, %s!, %s", output),
+	}
+
+	body, err := json.Marshal(response)
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(body),
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
